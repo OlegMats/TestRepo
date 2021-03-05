9526af8fa7450433f68a24f49504bd613d40a535
OlegMats <letmeshadowfiend@gmail.com>
2021-03-05 10:59:35 +0300

    local changes from DevB (editing of some methods and adding some comments)

--- Factories.java
+++ Factories.java
@@ -1,4 +1,5 @@
 //updates short variant of Factories.java
+//small refactoring of some methods
 interface Service {
     void method1();
     void method2();
@@ -8,9 +9,9 @@ interface ServiceFactory {
     Service getService();
 }
 
-class Implementation1 implements Service {
+class Implementation_B1 implements Service { // here you can see the first amend
 
-    Implementation1() {
+    Implementation_B1() {
     }
 
     public void method1() {
@@ -24,13 +25,13 @@ class Implementation1 implements Service {
 
 class Implementation1Factory implements ServiceFactory{
     public Service getService(){
-        return new Implementation1();
+        return new Implementation_B1();
     }
 }
 
-class Implementation2 implements Service {
+class Implementation_B2 implements Service { // another one change _ -> _B
 
-    Implementation2() {
+    Implementation_B2() {
     }
 
     public void method1() {
@@ -44,18 +45,19 @@ class Implementation2 implements Service {
 
 class Implementation2Factory implements ServiceFactory{
     public Service getService(){
-        return new Implementation2();
+        return new Implementation_B2();
     }
 }
-public class Factories2 {
-    public static void serviceConsumer(ServiceFactory fact) {
-        Service s = fact.getService();
-        s.method1();
-        s.method2();
+public class Factories {
+    public static void serviceConsumer(ServiceFactory serFact) {
+        Service ser = serFact.getService();
+        ser.method1();
+        ser.method2();
     }
 
     public static void main(String[] args) {
-        serviceConsumer(new Implementation1Factory());
-        serviceConsumer(new Implementation2Factory());
+        serviceConsumer(new Implementation_B1Factory());  // finally main
+        serviceConsumer(new Implementation_B2Factory());
     }
+    // the end
 }
