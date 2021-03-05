a2062c2e0676d3e3e2ca823151bf0937c6393b6a
OlegMats <letmeshadowfiend@gmail.com>
2021-03-05 08:59:51 +0300

    refactoring by DevB with providing additional info into comments.txt

--- Factories.java
+++ Factories.java
@@ -49,14 +49,14 @@ class Implementation2 implements Service {
 }
 
 public class Factories {
-    public static void serviceConsumer(ServiceFactory fact) {
-        Service s = fact.getService();
-        s.method1();
-        s.method2();
+    public static void serviceUser(ServiceFactory serFact) {
+        Service ser = serFact.getService();
+        ser.method1();
+        ser.method2();
     }
 
     public static void main(String[] args) {
-        serviceConsumer(Implementation1.factory);
-        serviceConsumer(Implementation2.factory);
+        serviceUser(Implementation1.factory);
+        serviceUser(Implementation2.factory);
     }
 }
